df0985883c34974364a29cad985a6081ad0fad80
Md Shawon Reza <fahadshawon241@gmail.com>
2023-10-18 20:50:20 +0600

    data post in database successfully

--- index.js
+++ index.js
@@ -28,6 +28,25 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const database = client.db("productsDB");
+        const productCollection = database.collection("product");
+        // const brandCollection = database.collection("brand");
+
+        app.post('/products', async (req, res) => {
+            const newProduct = req.body
+            console.log(newProduct)
+            const result = await productCollection.insertOne(newProduct);
+            res.send(result)
+        })
+        
+        app.get('/products',async(rwq,res)=>{
+            const cursor = productCollection.find();
+            const result= await cursor.toArray()
+            res.send(result)
+        })
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
